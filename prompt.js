c6658d0c4a6288a092f8051846ab29b06812764d
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-08-22 15:11:16 -0700

    added questions for the different types of employees

---
+++ prompt.js
@@ -0,0 +1,31 @@
+module.exports = {
+
+    manager: [
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'What is your ID?'
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is your email?'
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNum',
+            message: 'What is your office number?'
+        },
+    ]
+
+
+
+
+
+
+}
