15a9cf06673b532b67eaf7c948443d7840c96465
Caleb Kirkish <caleb.kirkish@gmail.com>
2020-08-23 20:40:10 -0700

    added questions for manager, intern, and engineer

--- prompt.js
+++ prompt.js
@@ -20,12 +20,50 @@ module.exports = {
             type: 'input',
             name: 'managerOfficeNum',
             message: 'What is your office number?'
+        }
+    ],
+    intern: [
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is your name?'
         },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'What is your ID?'
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is your email?'
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What university do you attend?'
+        }
+    ],
+    engineer: [
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: 'What is your ID?'
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is your email?'
+        },
+        {
+            type: 'input',
+            name: 'getGitHub',
+            message: 'What is your GitHub username?'
+        }
     ]
-
-
-
-
-
-
-}
+};
